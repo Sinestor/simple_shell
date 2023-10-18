bc24a63d4f2797e06acd9839e93012da9d6fcdd3
Sinestor <emmanuelblizz722@gail.com>
2023-10-18 02:50:11 -0700

    Write a beautiful code that passes the Betty checks

---
+++ simpl_code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main - a funtion that prints a phrase
+ * Return: 0
+ */
+
+int main(void)
+{
+        printf("hey!! Buddy, what's your name!:");
+        return (0);
+}
